2935c523c25815b35c10df13b93f9bcc70fb88c7
Janssen12 <jcmm2023-4700-61428@bicol-u.edu.ph>
2026-04-15 22:48:56 +0800

    Create connection.php

---
+++ api-server/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+// Database connection 
+$host = "localhost";
+$dbname = "user_api";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode([
+        "status" => "error",
+        "message" => "Database connection failed"
+    ]));
+}
+
+
+
+// Set headers for security and JSON handling
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
